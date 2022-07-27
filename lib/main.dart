34bfa3a4bf675642c8053b0c966bf76424864626
home-22 <armin.jasarevic.25@gmail.com>
2022-07-27 20:59:02 +0200

    Ticket Booking App

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:booking_app/home%20screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Booking App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
