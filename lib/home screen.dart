1871a2091ac467b2306dd6fe83b1a3cc64596d50
home-22 <armin.jasarevic.25@gmail.com>
2022-07-28 23:40:14 +0200

    Ticket Booking App

--- lib/home screen.dart
+++ lib/home screen.dart
@@ -8,22 +8,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int selectedIndex = 0;
+  List pages = [
+    const Text('Home'),
+    const Text('Search'),
+    const Text('Tickets'),
+    const Text('Profile'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Good morning'),
       ),
-      body: const Center(
-        child: Text('Book Tickets'),
+      body: Center(
+        child: pages[selectedIndex],
       ),
-      bottomNavigationBar: BottomNavigationBar(elevation: 10, items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.airplane_ticket), label: 'Ticket'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          elevation: 10,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+                backgroundColor: Colors.amber),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.airplane_ticket), label: 'Ticket'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+          ]),
     );
   }
 }
