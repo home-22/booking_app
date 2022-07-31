b1d6ec7faba2c3e4358471e10f6badd4de814430
home-22 <armin.jasarevic.25@gmail.com>
2022-07-31 03:33:09 +0200

    booking_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:booking_app/bottom_bar.dart';
+import 'package:booking_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const BottomBar(),
+      home: const HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,44 +13,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'Good morning',
-                          style: TextStyle(color: Colors.black45),
-                        ),
-                        Text(
-                          'Book Tickets',
-                          style: TextStyle(color: Colors.black45),
-                        ),
-                      ],
-                    ),
-                  ],
+      body: Container(
+        padding: const EdgeInsets.only(top: 40, left: 20),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Welcome',
+                  style: TextStyle(fontSize: 50),
                 ),
-                Container(
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: const DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage('images/logo.png'),
-                    ),
-                  ),
-                ),
+                Text(
+                  'aboard _____',
+                  style: TextStyle(fontSize: 30),
+                )
               ],
             ),
-          ),
-        ],
+          ],
+        ),
+        width: double.maxFinite,
+        height: double.maxFinite,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/home.png'), fit: BoxFit.cover),
+        ),
       ),
     );
   }
