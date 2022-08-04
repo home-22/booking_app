5425e27577fa4347eb2428aef5f01492509b23f0
home-22 <armin.jasarevic.25@gmail.com>
2022-08-04 23:30:15 +0200

    booking_app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,27 +13,75 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              height: double.infinity,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('images/home.png'), fit: BoxFit.fill),
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 350,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/home.png'), fit: BoxFit.cover),
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(),
-              ],
+            Positioned(
+              left: 10,
+              top: 30,
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.menu,
+                        color: Colors.amber,
+                        size: 30,
+                      ))
+                ],
+              ),
             ),
-          ),
-        ],
+            Positioned(
+              top: 320,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.grey[500],
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Your flight',
+                          style: TextStyle(fontSize: 30, color: Colors.amber),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.menu_open_outlined,
+                            size: 30,
+                            color: Colors.amber,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
